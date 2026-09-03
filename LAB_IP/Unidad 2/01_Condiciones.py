1fae3e51f37b92aaf27ce435242f4ab634bdfc81
GAEL MARTINEZ <gael5002mx@gmail.com>
2026-09-02 23:17:49 -0600

    CREATE Unidad 2

---
+++ LAB_IP/Unidad 2/01_Condiciones.py
@@ -0,0 +1,6 @@
+print(bool(1))
+print(bool(0))
+print(bool(-1))
+print(bool(10000))
+print(bool(10.5))
+print(bool("A")) #Todo valor distinto de 0, distinto de cadena vacia y distinto de None es True
---
+++ LAB_IP/Unidad 2/02_Operadores.py
@@ -0,0 +1,2 @@
+o1,op,o2=(input("Introduce el primer numero: ")),input("Introduce el operador: "),(input("Introduce el segundo numero: "))
+print(eval(o1+op+o2))
